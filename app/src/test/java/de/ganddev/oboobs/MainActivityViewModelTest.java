172b700cd2ccf482898044cf5061feac20d90c53
Bjoern Ahlfeld <3842688+ganddev@users.noreply.github.com>
2017-10-10 19:18:08 +0200

    Update Proguard rules for Okio Retrofit2 and Dagger2

---
+++ app/src/test/java/de/ganddev/oboobs/MainActivityViewModelTest.java
@@ -0,0 +1,45 @@
+package de.ganddev.oboobs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by bjornahlfeld on 10.10.17.
+ */
+public class MainActivityViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getBoobs() throws Exception {
+
+    }
+
+    @Test
+    public void onResponse() throws Exception {
+
+    }
+
+    @Test
+    public void onFailure() throws Exception {
+
+    }
+
+    @Test
+    public void loadNext() throws Exception {
+
+    }
+
+    @Test
+    public void swipedToRight() throws Exception {
+
+    }
+
+    @Test
+    public void swipedToLeft() throws Exception {
+
+    }
+
+}
